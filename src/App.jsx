f4088063bb335599fa8c458a2ccf5aee809cfa4d
Maria Emilia Pigini <piginimaemilia@gmail.com>
2023-10-28 23:12:49 +0200

    Creo el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoutesApp from "./routes/RoutesApp";
 
 function App() {
-
-
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<RoutesApp />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
