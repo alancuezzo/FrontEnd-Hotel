8a8e07fa62991ebebffa7725953562c13d8bca91
Maria Emilia Pigini <piginimaemilia@gmail.com>
2023-10-29 22:12:27 +0100

    Creo las rutas y sigo desarrollando el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
+// import "../css/home.css";
+import videoIntro from "../assets/VideoHome/VideoIntro.mp4"
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="fondovideo">
+        <video src={videoIntro} autoPlay loop muted />
+        <div class="capa"></div>
+      </div>
+      <div className="lala">LALA</div>
+    </>
   )
 }
 
