f8f1a4482815f0d3fee16f034a95103c4c4c6b20
Maria Emilia Pigini <piginimaemilia@gmail.com>
2023-11-02 20:00:25 +0100

    Creo estilos para las cards

--- src/components/CardOffers.jsx
+++ src/components/CardOffers.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "../css/cardsoffers.css";
 
 const CardOffers = ({card}) => {
     return (
         <>
             <div className="card text-center bg-dark">
-            <img className="movie-grid-image " src={card.image} alt={card.title}/>
+                <div className="overflow">
+                    <img className="card-img-top" src={card.image}/>
+                </div>
                 <div className="card-body ">
                     <h4 className="card-title text-light">
                         {card.title}
