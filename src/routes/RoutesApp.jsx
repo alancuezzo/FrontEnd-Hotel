aafd3b1f5510a191e6ab414f133cda48b03457a5
Maria Emilia Pigini <piginimaemilia@gmail.com>
2023-11-09 00:23:03 +0100

    Completo detalles del navbar

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/NavBarApp";
 
 const RoutesApp = () => {
   return (
