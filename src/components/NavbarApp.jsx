2cbc10af22749c1a4384e489fc49631b0aae0b94
Maria Emilia Pigini <piginimaemilia@gmail.com>
2023-11-09 11:48:58 +0100

    Agrego boton hamburguesa

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -6,7 +6,7 @@ import "../css/navbar.css";
 const NavBar = () => {
     return (
         <>
-            <nav>
+            <div className="navbar1">
                 <div className="nav_superior">
                     <Link to="*">ACCESO</Link>
                     <Link to="*">ÚNETE AHORA</Link>
@@ -49,7 +49,29 @@ const NavBar = () => {
                     </button>
                 </div>
                 <div></div>
-            </nav>
+            </div>
+            <div className="navbar2">
+            <section class="mb-3">
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <button class="navbar-toggler second-button" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#navbarToggleExternalContent10"
+        aria-controls="navbarToggleExternalContent10" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
+      </button>
+    </div>
+  </nav>
+  <div class="collapse" id="navbarToggleExternalContent10">
+    <div class="bg-light shadow-3 p-4">
+      <button class="btn btn-link btn-block border-bottom m-0">Link 1</button>
+      <button class="btn btn-link btn-block border-bottom m-0">Link 2</button>
+      <button class="btn btn-link btn-block m-0">Link 3</button>
+    </div>
+  </div>
+</section>
+            </div>
+
         </>
     )
 };
--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "../components/NavBarApp";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
 const RoutesApp = () => {
   return (
