423421e2fa83fb476be859b15def38af46094754
Alan Cuezzo <alanferrerocuezzo@gmail.com>
2023-11-14 12:10:59 -0300

    se agrego en el input de categoria los tipos que existen actualmente

--- src/components/InputCategoria.jsx
+++ src/components/InputCategoria.jsx
@@ -16,45 +16,25 @@ const InputCategoria = ({ categoriaSeleccionada }) => {
 
   return (
    
-      <div className="container-fluid">
-        <span className="navbar-brand">Categorías:</span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavCate"
-          aria-controls="navbarNavCate"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavCate">
+      
+     <>
           {categorias && (
-            <ul className="navbar-nav">
-              <li className="nav-item me-2">
-                <button
-                  className="btn btn-dark"
-                  onClick={() => categoriaSeleccionada("")}
-                >
-                  TODAS
-                </button>
-              </li>
+            < >
+             
               {categorias.map((categoria) => (
-                <li className="nav-item me-2" key={categoria._id}>
+                <option value={categoria} className=" " key={categoria._id}>
                   <button
                     className="btn btn-dark"
                     onClick={() => categoriaSeleccionada(categoria.nombre)}
                   >
                     {categoria.nombre}
                   </button>
-                </li>
+                </option>
               ))}
-            </ul>
+            </>
           )}
-        </div>
-      </div>
-   
+        
+        </>
   );
 };
 
